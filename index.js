bd21ae3935770422e4d571dc87292a08d5b32ad1
Moumen Hallak <moumen95@gmail.com>
2021-05-19 04:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here
+class Breakfast{
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructo(salad, soup, entree, dessert){
+    
+  }
+}
