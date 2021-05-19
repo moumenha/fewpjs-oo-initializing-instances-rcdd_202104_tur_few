c84563d70c9dd42bb43db27f8ee0aff8518c29ca
Moumen Hallak <moumen95@gmail.com>
2021-05-19 04:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ class Lunch {
 
 class Dinner {
   constructo(salad, soup, entree, dessert){
-    
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
   }
 }
